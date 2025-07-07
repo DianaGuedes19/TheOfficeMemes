b3fbe2ae14af79ea952abe617a54ecc45d3b5f08
Dianaguedes19 <1241903@isep.ipp.pt>
2025-07-07 14:51:11 +0100

    New Repository extending JPA. Ref #1

--- src/main/java/com/theOfficeMemes/TheOfficeMemes/repository/memesRepository.java
+++ src/main/java/com/theOfficeMemes/TheOfficeMemes/repository/memesRepository.java
@@ -3,8 +3,6 @@ package com.theOfficeMemes.TheOfficeMemes.repository;
 import com.theOfficeMemes.TheOfficeMemes.domain.Memes;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 
 public interface memesRepository extends JpaRepository<Memes,Long> {
-    List<Memes> getByID(Long id);
 }
