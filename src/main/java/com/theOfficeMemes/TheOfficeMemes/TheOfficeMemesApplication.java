055dcf228584afb95fdbbb9bd50c377c432c68cf
Dianaguedes19 <1241903@isep.ipp.pt>
2025-07-07 12:19:36 +0100

    First Commit

---
+++ src/main/java/com/theOfficeMemes/TheOfficeMemes/TheOfficeMemesApplication.java
@@ -0,0 +1,13 @@
+package com.theOfficeMemes.TheOfficeMemes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheOfficeMemesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheOfficeMemesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/theOfficeMemes/TheOfficeMemes/TheOfficeMemesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theOfficeMemes.TheOfficeMemes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheOfficeMemesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
