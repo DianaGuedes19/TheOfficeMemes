c07552cda41c274beabc19c2bf72ab5c73e1fb6d
Dianaguedes19 <1241903@isep.ipp.pt>
2025-07-07 13:59:30 +0100

    Decide to have an repository and add with an endpoint each meme to make this api easier to use. Ref #1

---
+++ src/main/java/com/theOfficeMemes/TheOfficeMemes/domain/Memes.java
@@ -0,0 +1,37 @@
+package com.theOfficeMemes.TheOfficeMemes.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Memes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String url;
+
+    // Constructor
+    public Memes(int id, String url) {
+        if (url == null || url.isBlank() || url.isEmpty()){
+            throw new IllegalArgumentException("URL cannot be null or Empty");
+        }
+        this.id = id;
+        this.url = url;
+    }
+
+    // Empty Constructor
+    public Memes() {
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+}
